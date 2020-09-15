d3d5f57619db96308a7f0b9232bca25bfc4c7dd9
nephinj <john.nephin@outlook.com>
2020-09-15 17:05:35 -0400

    fixed locals

--- main.tf
+++ main.tf
@@ -10,25 +10,3 @@ terraform {
   }
 }
 
-
-locals {
-
-  backupStorageName = "sqlbck${random_string.random.result}stg"
-
-  vmSettings = {
-    availabilitySets = {
-      sqlAvailabilitySetName = "${var.sqlServerConfig.vmName}-avs"
-    }
-    rdpPort = 3389
-  }
-  sqlAOEPName       = "${var.sqlServerConfig.vmName}-hadr"
-  sqlAOAGName       = "${var.sqlServerConfig.vmName}-ag"
-  sqlAOListenerName = "${var.sqlServerConfig.vmName}-lis"
-  sharePath         = "${var.sqlServerConfig.vmName}-fsw"
-  clusterName       = "${var.sqlServerConfig.vmName}-cl"
-  sqlwNicName       = "${var.witnessServerConfig.vmName}-nic"
-  keyVaultId        = "${data.azurerm_key_vault.keyvaultsecrets.id}"
-}
-
-
-
