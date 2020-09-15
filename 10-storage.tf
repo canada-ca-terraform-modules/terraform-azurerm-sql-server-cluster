99195865d3271c77975582db1f642741e9b81c0e
nephinj <john.nephin@outlook.com>
2020-09-15 18:07:30 -0400

    fixed name

--- 10-storage.tf
+++ 10-storage.tf
@@ -1,15 +1,11 @@
 
-locals {
-  backupStorageName = "sqlbck${random_string.random.result}stg"
-
-}
 
 #Create the diagnostic storage account
 module sqldiag {
   source                   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-storage_account?ref=v1.0.1"
   env                      = var.env
   userDefinedString        = "sqldiag"
-  resource_group           = local.resource_groups_L1.Logs
+  resource_group           = var.resource_group_name
   account_tier             = var.sqlServerConfig.storageAccountTier
   account_kind             = var.sqlServerConfig.storageAccountKind
   account_replication_type = var.sqlServerConfig.storageAccountReplicationType
@@ -20,8 +16,8 @@ module sqldiag {
 module sqlbackup {
   source                   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-storage_account?ref=v1.0.1"
   env                      = var.env
-  userDefinedString        = local.backupStorageName
-  resource_group           = local.resource_groups_L1.Logs
+  userDefinedString        = "sqlbck"
+  resource_group           = var.resource_group_name
   account_tier             = var.sqlServerConfig.storageAccountTier
   account_kind             = var.sqlServerConfig.storageAccountKind
   account_replication_type = var.sqlServerConfig.storageAccountReplicationType
