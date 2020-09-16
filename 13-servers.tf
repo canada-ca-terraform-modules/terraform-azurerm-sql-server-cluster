f2c3fb368b47efc5090a4275d01deec9e08f86ad
nephinj <john.nephin@outlook.com>
2020-09-16 13:36:03 -0400

    fixed storage

--- 13-servers.tf
+++ 13-servers.tf
@@ -1,17 +1,19 @@
 #Create the primary SQL server
 module "sqlvm1" {
-  source              = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-windows_virtual_machine?ref=v1.1.1"
-  env                 = var.env
-  userDefinedString   = var.sqlServerConfig.vmName
-  postfix             = "001"
-  resource_group      = var.resource_group_name
-  admin_username      = var.adminUsername
-  admin_password      = data.azurerm_key_vault_secret.localAdminPasswordSecret.value
-  subnet              = var.vnetConfig.dbSubnetName
-  availability_set_id = azurerm_availability_set.sqlAS.id
-  public_ip           = false
-  vm_size             = var.sqlServerConfig.vmSize
-  data_disk_sizes_gb  = [var.sqlServerConfig.dataDisks.diskSizeGB, var.sqlServerConfig.dataDisks.diskSizeGB]
+  source                 = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-windows_virtual_machine?ref=v1.1.1"
+  env                    = var.env
+  userDefinedString      = var.sqlServerConfig.vmName
+  postfix                = "001"
+  resource_group         = var.resource_group_name
+  admin_username         = var.adminUsername
+  admin_password         = data.azurerm_key_vault_secret.localAdminPasswordSecret.value
+  subnet                 = var.vnetConfig.dbSubnetName
+  availability_set_id    = azurerm_availability_set.sqlAS.id
+  public_ip              = false
+  vm_size                = var.sqlServerConfig.vmSize
+  data_disk_sizes_gb     = [var.sqlServerConfig.dataDisks.diskSizeGB, var.sqlServerConfig.dataDisks.diskSizeGB]
+  os_managed_disk_type   = lookup(var.sqlServerConfig, "os_managed_disk_type", "StandardSSD_LRS")
+  data_managed_disk_type = lookup(var.sqlServerConfig, "data_managed_disk_type", "StandardSSD_LRS")
   storage_image_reference = {
     publisher = var.sqlServerConfig.imageReference.sqlImagePublisher
     offer     = var.sqlServerConfig.imageReference.offer
@@ -35,8 +37,8 @@ module "sqlvm2" {
   public_ip              = false
   vm_size                = var.sqlServerConfig.vmSize
   data_disk_sizes_gb     = [var.sqlServerConfig.dataDisks.diskSizeGB, var.sqlServerConfig.dataDisks.diskSizeGB]
-  os_managed_disk_type   = lookup(var.vmConfigs.sqlServerConfig, "os_managed_disk_type", "StandardSSD_LRS")
-  data_managed_disk_type = lookup(var.vmConfigs.sqlServerConfig, "data_managed_disk_type", "StandardSSD_LRS")
+  os_managed_disk_type   = lookup(var.sqlServerConfig, "os_managed_disk_type", "StandardSSD_LRS")
+  data_managed_disk_type = lookup(var.sqlServerConfig, "data_managed_disk_type", "StandardSSD_LRS")
   storage_image_reference = {
     publisher = var.sqlServerConfig.imageReference.sqlImagePublisher
     offer     = var.sqlServerConfig.imageReference.offer
@@ -67,8 +69,8 @@ module "sqlvmw" {
     sku       = var.witnessServerConfig.imageReference.sku
     version   = var.witnessServerConfig.imageReference.version
   }
-  os_managed_disk_type   = lookup(var.vmConfigs.witnessServerConfig, "os_managed_disk_type", "StandardSSD_LRS")
-  data_managed_disk_type = lookup(var.vmConfigs.witnessServerConfig, "data_managed_disk_type", "StandardSSD_LRS")
+  os_managed_disk_type   = lookup(var.witnessServerConfig, "os_managed_disk_type", "StandardSSD_LRS")
+  data_managed_disk_type = lookup(var.witnessServerConfig, "data_managed_disk_type", "StandardSSD_LRS")
   tags                   = var.tags
 }
 
