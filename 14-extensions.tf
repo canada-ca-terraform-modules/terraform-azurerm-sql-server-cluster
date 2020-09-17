d91cb8424e5919cfa1956575fdd286180519086f
nephinj <john.nephin@outlook.com>
2020-09-17 18:31:35 -0400

    fixed depends on

--- 14-extensions.tf
+++ 14-extensions.tf
@@ -84,7 +84,7 @@ resource "azurerm_virtual_machine_extension" "CreateFailOverCluster" {
   publisher            = "Microsoft.Powershell"
   type                 = "DSC"
   type_handler_version = "2.71"
-  depends_on           = [azurerm_virtual_machine_extension.PrepareAlwaysOn, module.sqlvm2, azurerm_template_deployment.sqlvm]
+  depends_on           = [azurerm_virtual_machine_extension.PrepareAlwaysOn, module.sqlvm2, azurerm_template_deployment.sqlvm1, azurerm_template_deployment.sqlvm2]
   settings             = <<SETTINGS
             {
                 
