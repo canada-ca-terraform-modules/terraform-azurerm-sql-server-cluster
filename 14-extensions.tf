acca04626c7ee4c434ae625177834a16d2fb5dd1
nephinj <john.nephin@outlook.com>
2020-10-02 16:44:14 -0400

    update dsc

--- 14-extensions.tf
+++ 14-extensions.tf
@@ -171,7 +171,7 @@ resource "azurerm_virtual_machine_extension" "JoinFailOverCluster" {
             {
                 
                 "modulesURL": "https://raw.githubusercontent.com/canada-ca-terraform-modules/terraform-azurerm-sql-server-cluster/20200916.1/DSC/CreateFailoverCluster.ps1.zip",
-                "configurationFunction": "CreateFailoverCluster.ps1\\CreateFailoverCluster",
+                "configurationFunction": "SecondaryFailoverCluster.ps1\\SecondaryFailoverCluster",
                 "properties": {
                     "domainName": "${var.adConfig.domainName}",
                     "clusterName": "${local.clusterName}",
